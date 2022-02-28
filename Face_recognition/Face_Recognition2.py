a059ae8841c46fb0f217deab41deb02a8fdacd57
Nabarun Samaddar <70254017+Nabarun7@users.noreply.github.com>
2022-02-28 19:46:26 +0530

    Update Face_Recognition2.py

--- Face_recognition/Face_Recognition2.py
+++ Face_recognition/Face_Recognition2.py
@@ -12,12 +12,12 @@ imgTest = cv2.cvtColor(imgTest, cv2.COLOR_BGR2RGB)
 faceLoc = face_recognition.face_locations(imgElon)[0]
 encodeElon = face_recognition.face_encodings(imgElon)[0]
 
-cv2.rectangle(imgElon, (faceLoc[3],faceLoc[0]), (faceLoc[1], faceLoc[2]), (255, 0, 255), 2)
+cv2.rectangle(imgElon, (faceLoc[3],faceLoc[0]), (faceLoc[1], faceLoc[2]), (0, 255, 0), 2)
 
 faceLocTest = face_recognition.face_locations(imgTest)[0]
 encodeTest = face_recognition.face_encodings(imgTest)[0]
 
-cv2.rectangle(imgTest, (faceLocTest[3],faceLocTest[0]), (faceLocTest[1], faceLocTest[2]), (255, 0, 255), 2)
+cv2.rectangle(imgTest, (faceLocTest[3],faceLocTest[0]), (faceLocTest[1], faceLocTest[2]), (0, 255, 0), 2)
 
 results = face_recognition.compare_faces([encodeElon], encodeTest)
 
